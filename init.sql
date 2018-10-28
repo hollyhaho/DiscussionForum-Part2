d93abea64f5b7a132314899dff5039787c4aa4fe
Nguyen Tran <ntran272@csu.fullerton.edu>
2018-10-27 20:00:26 -0700

    Done! still doing read mine

---
+++ init.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS forums;
+DROP TABLE IF EXISTS threads;
+
+CREATE TABLE  IF NOT EXISTS users(
+	Id INTEGER PRIMARY KEY ASC, 
+	username TEXT, 
+	password TEXT
+);
+
+CREATE TABLE IF NOT EXISTS forums(
+	Id INTEGER PRIMARY KEY ASC, 
+	forum_name TEXT, 
+	forum_creator TEXT,
+	FOREIGN KEY (forum_creator) REFERENCES users(username)
+);
+
+CREATE TABLE IF NOT EXISTS threads(
+	Id GUID PRIMARY KEY,
+	thread_title TEXT NOT NULL,
+	thread_creator TEXT,
+	thread_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+	forum_Id INTEGER,
+	FOREIGN KEY (forum_Id) REFERENCES forums(Id),
+	FOREIGN KEY (thread_creator) REFERENCES users(username)
+);
+
+
+insert into threads (Id, thread_title, thread_creator, forum_id) values ('e8c3c68f-4557-450d-a4ad-f3d20cb50c10', 'Does anyone know how to start Redis?!!!!', 'bob', 1);
+insert into threads (Id, thread_title, thread_creator, forum_id) values ('417ef8c4-0bb1-450c-8bbb-e49b7656cbec', 'When is a good time for me to use Redis?', 'charlie', 1);
+insert into threads (Id, thread_title, thread_creator, forum_id) values ('ec93faf8-1bb7-4c3a-aa39-c8d6e909e93d', 'Why should I use mongodb over cassandra?', 'charlie', 2);
+insert into threads (Id, thread_title, thread_creator, forum_id) values ('7f308475-f1e4-46b4-8914-df347d6884ef', 'How do I start up flask?', 'charlie', 4);
+
+insert into forums (forum_name, forum_creator) values ('redis', 'alice');
+insert into forums (forum_name, forum_creator) values ('mongodb', 'bob');
+insert into forums (forum_name, forum_creator) values ('python', 'bob');
+insert into forums (forum_name, forum_creator) values ('flask', 'bob');
+
+insert into users (username, password) values ('holly', 'password');
+insert into users (username, password) values ('nguyen', 'password');
+insert into users (username, password) values ('bob', 'password');
+insert into users (username, password) values ('alice', 'password');
+insert into users (username, password) values ('bob', 'password');
+insert into users (username, password) values ('charlie', 'password');
---
+++ posts1.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE IF NOT EXISTS posts(
+	post_Id INTEGER PRIMARY KEY ASC,
+	post_text TEXT NOT NULL,
+	post_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+	post_authorId INTEGER,
+	post_forumId INTEGER,
+	post_threadId GUID);
+
+
+insert into posts (post_text, post_authorid, post_forumid, post_threadId) values ('I am having trouble connecting to Redis. Do you have any idea how to do it?','bob', 1, 'e8c3c68f-4557-450d-a4ad-f3d20cb50c10');
+insert into posts (post_text, post_authorid, post_forumid, post_threadId) values ('I Think you can google it', 'alice', 1, 'e8c3c68f-4557-450d-a4ad-f3d20cb50c10');
+insert into posts (post_text, post_authorid, post_forumid, post_threadId) values ('I want to use Edis for a project. Is it a good idea to use it for back end development?','holly', 1, '417ef8c4-0bb1-450c-8bbb-e49b7656cbec');
